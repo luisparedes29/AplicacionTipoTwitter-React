721bc23a9fac2aa06945cd85b8995ecd20798f19
Atilio García <atiliojg1@gmail.com>
2023-06-10 01:07:47 -0400

    Descripción de cambios
    CardTweet.jsx
    - Se le pasó la prop desde Tweets, que contiene los datos de todos los tweets, recuperado desde localStorage.
    - Con la prop, ahora el usuario y el tweet mostrados son dinámicos según la card.
    - Se pasa el prop data a EditarTweet.
    
    EditarTweet.jsx
    - Agregada lógica de edición. Funcional hasta el momento, falta hacer más testeo. También hace falta modificar ligeramente la estructura.

--- App-React-Twitter/src/components/CardTweet.jsx
+++ App-React-Twitter/src/components/CardTweet.jsx
@@ -1,21 +1,21 @@
-import {FaHeart, FaTrash} from "react-icons/fa"
+import { FaHeart, FaTrash } from "react-icons/fa";
 import EditarTweet from "./EditarTweet";
 
-function CardTweet(){
-    return(
+function CardTweet({ data }) {
+    return (
         <div>
             <section className="text-lg text-left p-4 border-b-2 border-white font-Quicksand">
-                <h1>Nombre</h1>
-                <p>tweetsssssssss</p>
+                <h1>{data.usuario}</h1>
+                <p>{data.tweet}</p>
 
                 <div className="flex justify-end w-full">
-                    <FaHeart className="mr-2 cursor-pointer"/>
-                    <FaTrash className="cursor-pointer mr-2"/>
-                    <EditarTweet/>
+                    <FaHeart className="mr-2 cursor-pointer" />
+                    <FaTrash className="cursor-pointer mr-2" />
+                    <EditarTweet data={data} />
                 </div>
             </section>
         </div>
-    )
+    );
 }
 
 export default CardTweet;
