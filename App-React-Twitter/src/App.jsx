837171c8a5812a821bf6288faf0dcfcfa46a3454
luisparedes29 <luisparedescrv@gmail.com>
2023-06-11 20:29:21 -0400

    Modificacion pasando estado de los tweets a un componente padre para Tweets y para crear Tweets

--- App-React-Twitter/src/App.jsx
+++ App-React-Twitter/src/App.jsx
@@ -2,12 +2,27 @@ import Header from "./components/Header";
 import Tweets from "./components/Tweets";
 import Footer from "./components/Footer";
 import ErrorBoundary from "./utilities/ErrorBoundaries";
+import CrearTweet from "./components/CrearTweet";
+import React, { useState, useEffect } from 'react';
+
 function App() {
+  const [tweets, setTweets] = useState(null)
+
+  useEffect(() => {
+      getTweets();
+  }, []);
+
+  const getTweets = async() => {
+      const dataExistente= await localStorage.getItem('tweets')
+      let parseados=JSON.parse(dataExistente)
+      setTweets(parseados.reverse())
+  };
     return (
         <div className="bg-azulGris min-h-screen flex-grow">
             <Header />
             <ErrorBoundary fallback = {"Algo salió mal. Inténtalo de nuevo más tarde"}>
-                <Tweets />
+                <Tweets tweets={tweets} actualizarTweets={getTweets} />
+                <CrearTweet actualizarTweets={getTweets}/>
             </ErrorBoundary>
             <Footer />
         </div>
--- App-React-Twitter/src/components/Tweets.jsx
+++ App-React-Twitter/src/components/Tweets.jsx
@@ -1,20 +1,9 @@
 import CardTweet from "./CardTweet"
-import React,{ useState, useEffect } from "react";
-import CrearTweet from "./CrearTweet";
+import React from "react";
 
 
-function Tweets(){
-    const [tweets, setTweets] = useState(null)
+function Tweets(props){
 
-    useEffect(() => {
-        getTweets();
-    }, []);
-
-    const getTweets = async() => {
-        const dataExistente= await localStorage.getItem('tweets')
-        let parseados=JSON.parse(dataExistente)
-        setTweets(parseados.reverse())
-    };
 
 
     return(
@@ -28,12 +17,11 @@ function Tweets(){
                 <h1 className="m-5 font-bold">Tweets</h1>
 
                 <section className="bg-azulOscuro2 rounded-t-lg w-full h-full">
-                    {tweets && tweets.map((data)=>(
-                        <CardTweet key={data.id} data={data} actualizarTweets={getTweets}/>
+                    {props.tweets && props.tweets.map((data)=>(
+                        <CardTweet key={data.id} data={data} actualizarTweets={props.actualizarTweets}/>
                     ))}
                 </section>
             </section>
-            <CrearTweet actualizarTweets={getTweets}/>
         </div>
     )
 }
